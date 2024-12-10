e3b2ce8b8d50c852360cdb07bd6b8aa35ef6b931
RPG512 <maxim-kotenko@hotmail.com>
2024-12-10 11:09:43 +0300

    Добавьте файлы проекта.

---
+++ HAKATON/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HAKATON.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
